d948dce61358eb64fe807cf149d4e823bbbe7317
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-12 09:54:30 +0530

    Update __main__.py

--- TelethonCat/__main__.py
+++ TelethonCat/__main__.py
@@ -13,7 +13,7 @@ from TelethonCat.utils.startup import (join_it, logger_check, start_msg,
 from TelethonCat.version import __hellver__
 
 # Global Variables #
-HELL_PIC = "https://te.legra.ph/file/cb0bd62632a3a2b6b2726.jpg"
+HELL_PIC = "https://graph.org/file/d62f4f344a871d06c8c37.jpg"
 
 
 # Client Starter
